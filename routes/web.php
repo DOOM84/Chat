76acc461978412018f6708c3916c5ccfcade9eca
Artem <d00m7@ukr.net>
2018-11-16 14:29:56 +0200

    Configuration of event, listener, pusher, laravel echo, initial work

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/', function () {
 });
 
 
-Route::get('chat', function (){
-    return view('chat');
-});
+Route::get('chat', 'ChatController@chat');
+Route::post('send', 'ChatController@send');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
